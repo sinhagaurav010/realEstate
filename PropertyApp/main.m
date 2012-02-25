837cfd9505b6f4e96e00f7e815ff83eeff7c2cf3
gaurav sinha <sinhagaurav010@gmail.com>
2012-02-26 02:41:13 +0530

    Initial Commit

---
+++ PropertyApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PropertyApp
+//
+//  Created by saurav sinha on 26/02/12.
+//  Copyright (c) 2012 sauravsinha007@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "testdelegateAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([testdelegateAppDelegate class]));
+    }
+}
---
+++ PropertyApp/testdelegateAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  testdelegateAppDelegate.h
+//  PropertyApp
+//
+//  Created by saurav sinha on 26/02/12.
+//  Copyright (c) 2012 sauravsinha007@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testdelegateAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
